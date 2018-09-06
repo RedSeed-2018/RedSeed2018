eb5d7cef885b39b5d04a3a5222aa8888b0c3109b
thaothu <thaothung.04@gmail.com>
2018-09-06 13:28:26 +0700

    jquery_accordion

---
+++ Desktop/js_front/jquery_accordion/1.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+	$('#chon').change(function(event) {
+		/* Act on the event */
+		$('.paragh').slideUp();
+		var value1='.paragh-'+$(this).val();
+		$(value1).toggle();
+	});
+	$('.paragh').slideUp();
+	
+	$('.container__title').click(function(event) {
+		/* Act on the event */
+		$('.paragh').slideUp();
+		$(this).next('.paragh').slideToggle(800);
+		
+		
+	});
+
+	
+});
+ 
