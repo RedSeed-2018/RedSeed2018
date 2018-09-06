79bcbf32fa3d4bd555a60d48866906c2f7bd9d1f
thaothu <thaothung.04@gmail.com>
2018-09-06 16:23:14 +0700

    boostrap_ex

---
+++ Desktop/js_front/boostrap_ex/1.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function() {
+	$('.form-inline').focus(function(event) {
+		/* Act on the event */
+		$('.form-inline').attr({
+			placeholder: 'Focusing... ', 
+			
+		});
+	});
+	
+	if($(window).width()>1128){
+		$(window).scroll(function(){                          
+            if ($(this).scrollTop() > $('.apartment-container').offset().top) {
+                $('.nav-header').fadeIn();
+               
+
+            } else {
+                $('.nav-header').fadeOut();
+            }
+        });
+	}
+		
+
+});
